6b05f8dd04e7040cdfeab9ab70627232a0d98bf9
Angela Paganucci <angela.luiz@db1.com.br>
2022-02-06 14:07:28 -0300

    Aula 59 - DevDojo

--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -1,11 +1,14 @@
 package academy.devdojo.maratonajava.javacore.Dconstrutores.test;
 
-import academy.devdojo.maratonajava.javacore.CsobrecargaMetodos.dominio.Anime;
+
+import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime();
-        anime.init("Akudama Drive", "TV", 12, "Ação");
+        Anime anime = new Anime("Teste nome", "Teste tipo", 12, "Teste genero", "Teste estudio");
+        Anime anime2 = new Anime();
         anime.imprime();
+        System.out.println("----------------");
+        anime2.imprime();
     }
 }
