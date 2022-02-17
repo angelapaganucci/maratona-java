f494285b57ea5d889969ab698314d1d7f99264c9
Angela Paganucci <angela.luiz@db1.com.br>
2022-02-16 21:53:22 -0300

    Aulda 78

--- src/academy/devdojo/maratonajava/javacore/JmodificadorFinal/dominio/Comprador.java
+++ src/academy/devdojo/maratonajava/javacore/JmodificadorFinal/dominio/Comprador.java
@@ -1,10 +1,15 @@
 package academy.devdojo.maratonajava.javacore.JmodificadorFinal.dominio;
 
-public class Carro {
+public class Comprador {
     private String nome;
-    public static final double VELOCIDADE_LIMITE = 250;
-    public final Comprador COMPRADOR = new Comprador();
-    
+
+    @Override
+    public String toString() {
+        return "Comprador{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
+
     public String getNome() {
         return nome;
     }
